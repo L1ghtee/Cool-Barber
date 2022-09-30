fc2e3e857cc5f2aeaedc130f38bb635467133d27
L1ghtee <nikorak20@gmail.com>
2022-09-30 14:24:33 +0300

    btns

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// menu
 const navMenu = document.querySelector("#navMenu");
 const menu = document.querySelector(".nav-menu");
 const body = document.querySelector("body");
@@ -8,40 +9,55 @@ navMenu.addEventListener("click", () => {
   body.classList.toggle("overflow-hidden");
 });
 
-let position =0;
-const slidesToShow= 4;
-const slidesToScroll= 1;
-const container = document.querySelector('.slider-container');
-const track = document.querySelector('.slider-track');
-const btnPrev= document.querySelector('.btn-prev');
-const btnNext= document.querySelector('.btn-next');
-const items = document.querySelectorAll('.slider-item');
+// close menu on click menu item
+const navlinks = document.querySelector(".nav-menu").children
+
+for(const navlink of navlinks){
+    navlink.addEventListener('click', function(event){
+        navMenu.classList.remove("active");
+        menu.classList.remove("active");
+        body.classList.remove("overflow-hidden");
+    })
+}
+
+//slider
+let position = 0;
+const slidesToShow = 4;
+const slidesToScroll = 1;
+const container = document.querySelector(".slider-container");
+const track = document.querySelector(".slider-track");
+const btnPrev = document.querySelector(".btn-prev");
+const btnNext = document.querySelector(".btn-next");
+const items = document.querySelectorAll(".slider-item");
 const itemsCount = items.length;
 const itemWidth = container.clientWidth / slidesToShow;
 const movePosition = slidesToScroll * itemWidth;
 
-items.forEach((item)=>{
-    item.style.minWidth= '${itemWidth}px';
+items.forEach((item) => {
+  item.style.minWidth = "${itemWidth}px";
 });
 
-btnPrev.addEventListener('click', ()=>{
-    const itemsLeft= itemsCount - (Math.abs(position)+ slidesToShow * itemWidth)/ itemWidth;
-    position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
-    setPosition();
-    checkBtns();
+btnPrev.addEventListener("click", () => {
+  const itemsLeft =
+    itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
+  position -=
+    itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+  setPosition();
+  checkBtns();
 });
-btnNext.addEventListener('click', ()=>{
-    const itemsLeft=Math.abs(position)/itemWidth;
-    position += itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
-    setPosition();
-    checkBtns();
+btnNext.addEventListener("click", () => {
+  const itemsLeft = Math.abs(position) / itemWidth;
+  position +=
+    itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+  setPosition();
+  checkBtns();
 });
 
-const setPosition=()=>{
-    track.style.transform= 'translateX(${position}px)';
+const setPosition = () => {
+  track.style.transform = "translateX(${position}px)";
 };
 
-const checkBtns= ()=>{
-    btnPrev.disabled = position === 0;
-    btnNext.disabled = position <= -(itemsCount - slidesToShow) * itemWidth;
-}
+const checkBtns = () => {
+  btnPrev.disabled = position === 0;
+  btnNext.disabled = position <= -(itemsCount - slidesToShow) * itemWidth;
+};
