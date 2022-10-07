24f9143fcedc28a0fcdb00185c07b83fbec0c429
L1ghtee <nikorak20@gmail.com>
2022-10-07 15:52:49 +0300

    try to adapt

--- js/app.js
+++ js/app.js
@@ -70,3 +70,16 @@ const checkBtns = () => {
   btnPrev.disabled = position === 0;
   btnNext.disabled = position <= -(itemsCount - slidesToShow) * itemWidth;
 };
+
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-121px";
+  }
+  prevScrollpos = currentScrollPos;
+}
